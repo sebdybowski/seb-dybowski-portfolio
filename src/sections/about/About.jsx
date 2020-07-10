0b8bb7155c2d96392844337217849b64b847a241
sebdybowski <dybowski@int.pl>
2020-07-11 01:57:47 +0200

    about prototyping 2

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -5,12 +5,12 @@ import { Portrait } from './Portrait';
 import { SPACING, TEXT } from '../../styles';
 
 export const About = () => <SectionLayout>
-  <div style={{ display: 'flex-inline', border: `8px solid ${TEXT.LIGHT}`, borderRadius: SPACING.LG, width: '100%', padding: '32px 48px', height: '30vh' }}>
+  <div style={{ display: 'flex-inline', boxSizing: 'border-box', border: `8px solid ${TEXT.LIGHT}`, borderRadius: SPACING.LG, width: '100%', padding: '48px 64px', height: '40vh' }}>
     <div>
       <MainHeading>{'Hi, I’m Seb'}</MainHeading>
       <SubHeading>Nice to meet You!</SubHeading>
     </div>
-    <div style={{ maxWidth: '768px', marginBottom: '1.45rem', marginLeft: 'auto', position: 'relative', top: '-356px', right: '-20px' }}>
+    <div style={{ maxWidth: '768px', marginBottom: '1.45rem', marginLeft: 'auto', position: 'relative', top: '-322px', right: '-20px' }}>
       <Portrait/>
     </div>
   </div>
